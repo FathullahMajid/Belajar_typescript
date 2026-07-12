736e481bc3e5566d74cd72134fd25f4f2dae2d23
Fathullah Majid <majidfathullah@gmail.com>
2026-07-12 18:13:33 +0700

    Tipe data string

---
+++ Variabel.ts
@@ -0,0 +1,44 @@
+1+2+4+8+16+32+64+128
+console.log(1+2+4+8+16+32+64+128)
+console.log('kebahagiaan hanya dalam iman')
+
+
+
+let nama: string = "Andi hhhuh gy gyg yg ygggyggygygyh gybty";
+//nama = "budi";
+console.log(nama); // budi
+
+
+
+let angka: string = "73";
+//angka = "1001";
+console.log(angka); // 1001
+
+
+
+let huruf: string = "ABCDE";
+huruf = "HIJKL";
+console.log(huruf); // HIJKL
+
+
+let saldo : number = "huuih";
+saldo = 20000;
+saldo = "OPQRS"
+console.log(saldo);
+
+
+let makanan: string = "Pempek";
+makanan = "Mie Yamin";
+console.log(makanan);  //Mie Yamin
+
+let amalan: string = "Dakwah";
+//amalan = "Taklim"
+console.log(amalan); //Taklim
+
+
+let finansial: string = "forex";
+finansial = "crypto";
+console.log(finansial); //crypto
+
+
+
