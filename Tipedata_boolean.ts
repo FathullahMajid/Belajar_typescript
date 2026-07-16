5584dfef04909cff31c70def6b460a4a40fcfdab
Fathullah Majid <majidfathullah@gmail.com>
2026-07-16 10:17:05 +0700

    belajar tipedata boolean

---
+++ Tipedata_boolean.ts
@@ -0,0 +1,30 @@
+let sudahSholat = true;
+let belumSholat = false;
+console.log(sudahSholat)
+
+console.log(10 > 5);
+console.log(10 < 5);
+console.log(8 === 8);
+console.log(8 === 10);
+
+let a: number = 20;
+let b: number = 40;
+let c: number = 30;
+console.log(a+b+c === 90);
+
+let d: number = 70;
+let e: number = 10;
+let f: number = 50;
+console.log(b+d-f === 60);
+
+let mandi = false;
+let makan = true;
+console.log(mandi);
+
+let g: number = 55;
+let h: number = 89;
+console.log(g !== h);
+
+console.log(a<g);
+console.log(e === c);
+console.log(c+f === 80)
