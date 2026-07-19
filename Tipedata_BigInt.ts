337934eb549e2661715cdbec03144d1c3bcacb27
Fathullah Majid <majidfathullah@gmail.com>
2026-07-19 19:58:32 +0700

    Belajar typedata BigInt

---
+++ Tipedata_BigInt.ts
@@ -0,0 +1,17 @@
+let jumlahBintang: bigint = 2536271726256127n;
+console.log(jumlahBintang);
+
+let angka: bigint = 100n;
+console.log(angka)
+
+let jumlahPenduduk: bigint = 8000000000n;
+jumlahPenduduk = 10000000000n;
+console.log(jumlahPenduduk);
+
+let a: bigint = 1000n;
+let b: bigint = 250n;
+console.log(a / b);
+
+let saldoThimoty: bigint = 13000000000000n;
+let saldoKoandrew: bigint = 5000000000000n;
+console.log(saldoThimoty+saldoKoandrew)
